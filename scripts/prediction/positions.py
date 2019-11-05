4f41e9af6231c1208410271beda0f487fe0cbfb1
Geir Ulvik <ulvik@usit.uio.no>
2019-11-05 18:09:26 +0100

    Updated positions. Works.

--- scripts/prediction/positions.py
+++ scripts/prediction/positions.py
@@ -5,67 +5,74 @@ from tensorflow.keras.utils import to_categorical
 import sys
 import matplotlib.pyplot as plt
 from master_data_functions.functions import *
-
+from master_models.prediction import *
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend
 
-# ==== Import Data ====
-images = load_feature_representation("images_200k.npy")
-energies = load_feature_representation("energies_200k.npy")
-positions = load_feature_representation("positions_200k.npy")
-labels = load_feature_representation("labels_200k.npy")
+# PATH variables
+DATA_PATH = "../../data/simulated/"
+OUTPUT_PATH = "../../data/output/"
+MODEL_PATH = OUTPUT_PATH + "models/"
 
-n_classes = labels.shape[1]
-print("Number of classes: {}".format(n_classes))
-print("Images shape: {}".format(images.shape))
-print("Energies shape: {}".format(energies.shape))
-print("Positions shape: {}".format(positions.shape))
-print("Labels shape: {}".format(labels.shape))
+# ================== Import Data ==================
+images = np.load(DATA_PATH + "images_noscale_200k.npy")
+positions = np.load(DATA_PATH + "positions_noscale_200k.npy")
+#labels = np.load(DATA_PATH + "labels_noscale_200k.npy")
 
+# ================== Prepare Data ==================
 
-# ==== Set up model ====
+# Set positions x2,y2 for single events to -1 instead of -100
+# Plan is to predict two positions regardless of single or double
+# event, but for single events x2,y2 should be predicted out of bounds.
+single_indices, double_indices, close_indices = event_indices(positions)
+positions[single_indices, 2:] = -1.0
 
-# Set up sequetial model based on Keras CIFAR-10 example
-model = Sequential()
-
-# Add layers
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-
-#model.add(Conv2D(64, (3, 3), padding='same'))
-#model.add(Activation('relu'))
-#model.add(Conv2D(64, (3, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-
-model.add(Flatten())
-#model.add(Dense(512))
-#model.add(Activation('relu'))
-#model.add(Dropout(0.5))
-model.add(Dense(4))
-model.add(Activation('linear'))
+# Split indices into training and test sets
+x_idx = np.arange(images.shape[0])
+train_idx, test_idx, not_used1, not_used2 = train_test_split(
+        x_idx, 
+        x_idx, 
+        test_size = 0.2
+        )  
 
 
-
-# Custom definition of R2 score for metrics
+# ================== Custom Functions ==================
+# Define R2 score for metrics since it's not available by default
 def r2_keras(y_true, y_pred):
     SS_res =  backend.sum(backend.square(y_true - y_pred)) 
     SS_tot = backend.sum(backend.square(y_true - backend.mean(y_true))) 
     return ( 1 - SS_res/(SS_tot + backend.epsilon()) )
+
+
+# ================== Model ==================
+net = "position"
+model = position_model()
+
+# Setup callback for saving models
+fpath = MODEL_PATH + net + "-r2-{r2_keras:.2f}.hdf5"
+cb = tf.keras.callbacks.ModelCheckpoint(
+        filepath=fpath, 
+        monitor='r2_keras', 
+        save_best_only=True
+        )
 
 # Compile model
 model.compile(loss='mse',
               optimizer='adam',
               metrics=[r2_keras])
 
-# Output summary of model
-print(model.summary())
+# Parameters for the model
+batch_size = 32
+epochs = 10
+
+history = model.fit(
+        images[train_idx],
+        positions[train_idx],
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(images[test_idx], positions[test_idx]),
+        callbacks=[cb])
 
