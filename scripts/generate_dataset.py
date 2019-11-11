b8b6c23d3ee931039c61442076df7dc6b67a9275
Geir Ulvik <ulvik@usit.uio.no>
2019-11-11 12:22:59 +0100

    Adding datageneration set

---
+++ scripts/generate_dataset.py
@@ -0,0 +1,18 @@
+import numpy as np
+from master_data_functions.functions import import_data
+
+
+# Generate a dataset of 1M samples
+
+DATA_PATH = "../data/simulated/"
+data = import_data(path=DATA_PATH, filename="CeBr2Mil_Mix.txt", num_samples=1E6)
+print("Data imported")
+print("Saving energies")
+np.save(DATA_PATH+"energies_1M.npy", data["energies"])
+print("Saving positions")
+np.save(DATA_PATH+"positions_1M.npy", data["positions"])
+print("Saving images")
+np.save(DATA_PATH+"images_1M.npy", data["images"])
+print("Saving labels")
+np.save(DATA_PATH+"labels_1M.npy", data["labels"])
+
