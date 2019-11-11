2434b0771feb0f10b90b52b4680e593bab4b5e6c
Geir Ulvik <ulvik@usit.uio.no>
2019-11-11 15:19:06 +0100

    Adding data-leaning script to remove nan values.

---
+++ scripts/clean_data.py
@@ -0,0 +1,37 @@
+from master_data_functions.functions import *
+import numpy as np
+
+filepath="../data/simulated/CeBr2Mil_Mix.txt"
+
+separated_data = {}
+
+# Temporary initialization of arrays-to-be
+images = []
+energies = []
+positions = []
+labels = []
+
+# read line by line to alleviate memory strain when files are large
+with open(filepath, "r") as infile:
+    for line in infile:
+        line = np.fromstring(line, sep=' ')
+        image, energy, position = separate_simulated_data(line, False)
+        label = label_simulated_data(line)
+
+        images.append(image)
+        energies.append(energy)
+        positions.append(position)
+        labels.append(label)
+
+# Convert lists to numpy arrays and reshape them to remove the added axis from
+# conversion. TODO: Find a better way to do this?
+images = np.array(images)
+print(np.amax(images))
+print(np.amin(images))
+#energies = np.array(energies)
+#positions = np.array(positions)
+#labels = np.array(labels)
+
+#images = images.reshape(images.shape[0], images.shape[2], images.shape[3], images.shape[4])
+#energies = energies.reshape(energies.shape[0], energies.shape[2])
+#positions = positions.reshape(positions.shape[0], positions.shape[2])
