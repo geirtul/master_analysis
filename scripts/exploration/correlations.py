17e4912e6ffaeb965c31ec8f9969cbcf5ebf3aa5
Geir Ulvik <ulvik@usit.uio.no>
2019-11-14 11:56:36 +0100

    Now producing plots

--- scripts/exploration/correlations.py
+++ scripts/exploration/correlations.py
@@ -13,7 +13,7 @@ MODEL_PATH = OUTPUT_PATH + "models/"
 images = np.load(DATA_PATH + "images_noscale_200k.npy")
 positions = np.load(DATA_PATH + "positions_noscale_200k.npy")
 energies = np.load(DATA_PATH + "energies_noscale_200k.npy")
-images = normalize_image_data(images)
+#images = normalize_image_data(images)
 #images = np.load(DATA_PATH + "images_1M.npy")
 #positions = np.load(DATA_PATH + "positions_1M.npy")
 #labels = np.load(DATA_PATH + "labels_noscale_200k.npy")
@@ -22,11 +22,19 @@ images = normalize_image_data(images)
 images = images.reshape(images.shape[0], 256)
 data = np.concatenate((images, positions, energies), axis=1)
 
-corr_matrix = np.corrcoeff(data, rowvar=False)
-
-plt.matshow(corr_matrix)
+corr_matrix = np.corrcoef(data, rowvar=False)
+plt.imshow(corr_matrix)
 plt.colorbar()
-plt.show()
+plt.savefig("corr_matrix.pdf")
+plt.clf()
 
+# Correlations for positions and energy, double events
+single_idx, double_idx, close_idx = event_indices(positions)
+corr_matrix_pos_energy = np.corrcoef(data[double_idx, 256:], rowvar=False)
+
+plt.imshow(corr_matrix_pos_energy)
+plt.colorbar()
+plt.savefig("corr_matrix_pos_energy.pdf")
+plt.clf()
 
 
