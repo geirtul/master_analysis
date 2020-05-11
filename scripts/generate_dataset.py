237e7f6049ad4b2ff7cfbb59a6ef4369c8545a67
Geir Tore Ulvik <geirtul@ml1.uio.no>
2020-05-11 09:45:07 +0200

    Updated dataset generation

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -1,19 +1,40 @@
 import numpy as np
-from master_data_functions.functions import import_data
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
+from master_data_functions.functions import import_data, event_indices
 
 
-# Generate a dataset of 1M samples
+# Generate a dataset of desired number of samples
 
 DATA_PATH = "../data/simulated/"
 fname = "CeBr2Mil_Mix.txt"
-data = import_data(DATA_PATH+fname, num_samples=5E3, scaling=False)
+data = import_data(DATA_PATH+fname, scaling=False)
 print("Data imported")
-print("Saving energies")
-np.save(DATA_PATH+"energies_20k.npy", data["energies"])
-print("Saving positions")
-np.save(DATA_PATH+"positions_20k.npy", data["positions"])
-print("Saving images")
-np.save(DATA_PATH+"images_20k.npy", data["images"])
-print("Saving labels")
-np.save(DATA_PATH+"labels_20k.npy", data["labels"])
 
+singles, doubles, close = event_indices(data["positions"][:1900000])
+singles2, doubles2, close2 = event_indices(data["positions"][1900000:])
+
+print("Num singles:", len(singles))
+print("Num doubles:", len(doubles))
+print("Num close:", len(close))
+print("Test set")
+print("Num singles:", len(singles2))
+print("Num doubles:", len(doubles2))
+print("Num close:", len(close2))
+print("Saving energies")
+
+np.save(DATA_PATH+"energies_full.npy", data["energies"][:1900000])
+print("Saving positions")
+np.save(DATA_PATH+"positions_full.npy", data["positions"][:1900000])
+print("Saving images")
+np.save(DATA_PATH+"images_full.npy", data["images"][:1900000])
+print("Saving labels")
+np.save(DATA_PATH+"labels_full.npy", data["labels"][:1900000])
+print("Saving test set ================")
+np.save(DATA_PATH+"energies_test.npy", data["energies"][1900000:])
+print("Saving positions")
+np.save(DATA_PATH+"positions_test.npy", data["positions"][1900000:])
+print("Saving images")
+np.save(DATA_PATH+"images_test.npy", data["images"][1900000:])
+print("Saving labels")
+np.save(DATA_PATH+"labels_test.npy", data["labels"][1900000:])
