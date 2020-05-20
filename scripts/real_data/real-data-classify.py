372bcdffb3eb894185582a3cf69e70656a78855d
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2020-05-20 15:38:52 +0200

    Added markdown priting to results table

--- scripts/real_data/real-data-classify.py
+++ scripts/real_data/real-data-classify.py
@@ -12,8 +12,8 @@ from tqdm import tqdm
 # ## Data import
 
 config = {
-    "DATA_PATH": "../../data/real/",
-    "DATA_FILENAME": "anodedata_sample.txt",
+    "DATA_PATH": "../../data/real/anodedata_short.txt",
+    "DATA_FILENAME": "anodedata_short.txt",
     "SAMPLE_PATH": "../../data/sample/CeBr10k_1.txt",
     "MODEL_PATH": "../../data/output/models/",   
     "OUTPUT_PATH": "../../data/output/",
@@ -65,12 +65,34 @@ ax.set_ylabel("Number of events")
 ax.legend()
 fig.savefig("num_events_per_descriptor.pdf")
 
+translate_descriptor = {
+    1: "Implant",
+    2: "Decay",
+    3: "implant + Decay",
+    4: "Light ion",
+    5: "Implant + Light Ion",
+    6: "Decay + Light Ion",
+    7: "Implant + Decay + Light Ion",
+    8: "Double (time)",
+    9: "Implant + Double (time)",
+    10: "Decay + Double (time)",
+    11: "Implant + Decay + Double (time)",
+    12: "Light ion + Double (time)",
+    13: "Implant + Light Ion + Double (time)",
+    14: "Decay + Light ion + Double (time)",
+    15: "Implant + Decay + Light Ion + Double (time)",
+    16: "Double (space)",
+    17: "Implant + Double (space)",
+    18: "Decay + Double (space)"
+}
 # Print a table-like structure for viewing
 print("Classification results:")
-print("Event descriptor | singles | doubles |")
+print("|Event descriptor | Event type                   | singles | doubles |")
+print("| :---           |  :---:                       | :---:   | :---:   |")
 for d in descriptors:
-    print("{:^17d}|{:^9d}|{:^9d}|".format(
-        d, 
+    print("|{:^17d}|{:^30s}|{:^9d}|{:^9d}|".format(
+        d,
+        translate_descriptor[d],
         desc_class['single'].count(d), 
         desc_class['double'].count(d)
         )
@@ -79,4 +101,5 @@ for d in descriptors:
 out_filename = config['OUTPUT_PATH'] + "events_classified_" + config["DATA_FILENAME"][:-4] + ".json"
 with open(out_filename, 'w') as fp:
     json.dump(events, fp, sort_keys=True, indent=4)
+print("Output file to: ", out_filename)
 
