7ca1595f1ea1946bd3bad47de881669f42ce7152
Geir Ulvik <g.t.ulvik@fys.uio.no>
2020-08-06 16:15:09 +0200

    Testing batch size variations

--- scripts/classification/hparam_classification.py
+++ scripts/classification/hparam_classification.py
@@ -32,71 +32,49 @@ train_idx, val_idx, u1, u2 = train_test_split(
 )
 
 # ================== Search params ================
-num_filters = [4, 8, 16, 32, 64]
-dense_sizes = [32, 64, 128, 256]
+batch_sizes = [32, 64, 128, 256]
 
 # set tf random seed
 tf.random.set_seed(config['random_seed'])
 id_param = {}
-search_name = "architecture_search_seeded_2conv"
+search_name = "batch_size_deeper"
 with tf.device(get_tf_device(20)):
-    # Build models
-    models = []
-    for n_filters1 in num_filters:
-        for n_filters2 in num_filters:
-            for d_size in dense_sizes:
-                model = Sequential()
-                model.add(
-                    Conv2D(
-                        filters=n_filters1,
-                        kernel_size=(3, 3),
-                        input_shape=images.shape[1:],
-                        padding='same',
-                        activation='relu',
-                    )
-                )
-                model.add(MaxPool2D(padding='same'))
-                model.add(
-                    Conv2D(
-                        filters=n_filters2,
-                        kernel_size=(3, 3),
-                        input_shape=images.shape[1:],
-                        padding='same',
-                        activation='relu',
-                    )
-                )
-                model.add(Flatten())
-                model.add(Dense(d_size, activation='relu'))
-                model.add(Dense(1, activation='sigmoid'))
+    for b_size in batch_sizes:
+        config['fit_args']['batch_size'] = b_size
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                         activation='relu', input_shape=(16, 16, 1)))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
 
-                model.compile(
-                    optimizer='adam',
-                    loss='binary_crossentropy',
-                    metrics=['accuracy']
-                )
+        model.compile(
+            optimizer='adam',
+            loss='binary_crossentropy',
+            metrics=['accuracy']
+        )
 
-                models.append(model)
-
-                # Run experiment
-                experiment = Experiment(
-                    model=model,
-                    config=config,
-                    model_type="classification",
-                    experiment_name=search_name
-                )
-                experiment.run(
-                    normalize_image_data(images[train_idx]),
-                    labels[train_idx],
-                    normalize_image_data(images[val_idx]),
-                    labels[val_idx],
-                )
-                experiment.save()
-                id_param[experiment.experiment_id] = {
-                    'l1_filters': n_filters1,
-                    'l2_filters': n_filters2,
-                    'kernel_size': (3, 3),
-                    'dense_size': d_size,
-                }
+        # Run experiment
+        experiment = Experiment(
+            model=model,
+            config=config,
+            model_type="classification",
+            experiment_name=search_name
+        )
+        experiment.run(
+            normalize_image_data(images[train_idx]),
+            labels[train_idx],
+            normalize_image_data(images[val_idx]),
+            labels[val_idx],
+        )
+        experiment.save()
+        id_param[experiment.experiment_id] = {
+            'batch_size': b_size,
+        }
 search_path = get_git_root() + "experiments/searches/"
 with open(search_path + search_name + ".json", "w") as fp:
     json.dump(id_param, fp, indent=2)
