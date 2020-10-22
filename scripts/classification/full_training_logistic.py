67fe7a26ef1b7343702089857ada007ed3bdac8e
Geir Tore Ulvik <geirtul@ml2.uio.no>
2020-10-22 19:15:45 +0200

    Adding specific scripts for thesis result generation

---
+++ scripts/classification/full_training_logistic.py
@@ -0,0 +1,68 @@
+# Imports
+from master_scripts.classes import Experiment
+from master_scripts.data_functions import (normalize_image_data, get_tf_device,
+                                           get_git_root,
+					   normalize_image_data_elementwise)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (Conv2D, Dense, Flatten, MaxPooling2D,
+                                     Dropout)
+import tensorflow as tf
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+# ================== Config =======================
+config = {
+    'fit_args': {
+        'epochs': 20,
+        'batch_size': 64,
+    },
+    'random_seed': 120,
+    'data': {
+        'images': "images_full.npy",
+        'labels': "labels_full.npy",
+    },
+}
+
+
+# ================== Callbacks ====================
+
+
+# ================== Import Data ==================
+DATA_PATH = get_git_root() + "data/simulated/"
+images = np.load(DATA_PATH + config['data']['images'])
+images = images.reshape(images.shape[0], 16, 16, 1)
+labels = np.load(DATA_PATH + config['data']['labels'])
+
+# log-scale the images if desireable
+config['scaling'] = "minmax"
+if "np.log" in config['scaling']:
+    images = np.log1p(images)
+
+# set tf random seed
+tf.random.set_seed(config['random_seed'])
+with tf.device(get_tf_device(20)):
+    # Build model
+    model = Sequential()
+    model.add(InputLayer(input_shape=(256,)))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    # Run experiment
+    experiment = Experiment(
+        model=model,
+        config=config,
+        model_type="classification",
+        experiment_name="full_training_logistic"
+    )
+    experiment.run(
+        normalize_image_data(images),
+        labels,
+    )
+    experiment.save()
+    mpath = experiment.config['path_args']['models'] + experiment.id + ".h5"
+    model.save(mpath)
