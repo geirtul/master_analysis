8f1aaed5e01ac1cc09f2672bb5a6814db338dc0d
Geir Ulvik <g.t.ulvik@fys.uio.no>
2020-10-24 21:21:38 +0200

    adding current progress

---
+++ scripts/classification/full_training_dense.py
@@ -0,0 +1,68 @@
+# Imports
+from master_scripts.classes import Experiment
+from master_scripts.data_functions import (normalize_image_data, get_tf_device,
+                                           get_git_root)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, InputLayer
+import tensorflow as tf
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+# ================== Config =======================
+config = {
+    'fit_args': {
+        'epochs': 3,
+        'batch_size': 64,
+    },
+    'random_seed': 120,
+    'data': {
+        'images': "images_full.npy",
+        'labels': "labels_full.npy",
+    },
+    'kfold_args': {
+        'shuffle': False,
+    }
+}
+
+# ================== Callbacks ====================
+
+
+# ================== Import Data ==================
+DATA_PATH = get_git_root() + "data/simulated/"
+images = np.load(DATA_PATH + config['data']['images'])
+images = images.reshape(images.shape[0], 16, 16, 1)
+labels = np.load(DATA_PATH + config['data']['labels'])
+
+# log-scale the images if desireable
+config['scaling'] = "minmax"
+if "np.log" in config['scaling']:
+    images = np.log1p(images)
+
+# set tf random seed
+tf.random.set_seed(config['random_seed'])
+with tf.device(get_tf_device(20)):
+    # Small Dense network
+    model = Sequential()
+    model.add(InputLayer(input_shape=(256,)))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    # Run experiment
+    experiment = Experiment(
+        model=model,
+        config=config,
+        model_type="classification",
+        experiment_name="full_training_dense_small"
+    )
+    experiment.run_kfold(
+        normalize_image_data(images).reshape(images.shape[0], 256),
+        labels,
+    )
+    experiment.save(save_model=True, save_indices=False)
+    print("Finished experiment:", experiment.id)
