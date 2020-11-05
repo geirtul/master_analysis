acd547d37021dba76dc9e8085a4054f0b2dbbf5e
Geir Ulvik <g.t.ulvik@fys.uio.no>
2020-11-05 22:09:11 +0100

    Fixed output size

--- scripts/regression/results_pos_single_linreg.py
+++ scripts/regression/results_pos_single_linreg.py
@@ -38,7 +38,7 @@ tf.random.set_seed(config['random_seed'])
 with tf.device(get_tf_device(20)):
     padding = 'same'
     model = Sequential()
-    model.add(Dense(1, activation='relu', input_shape=(256,)))
+    model.add(Dense(2, activation='relu', input_shape=(256,)))
     model.compile(
         loss='mse',
         optimizer='adam',
